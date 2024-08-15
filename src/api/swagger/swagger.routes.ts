6d132e8fc2c4365b8a19e7b0ce8b98f618ab54d6
Slava Volkov <boozybats@MacBook-Pro-Slava.local>
2024-08-15 15:08:55 +0500

    remove unneccesary logs

--- src/api/swagger/swagger.routes.ts
+++ src/api/swagger/swagger.routes.ts
@@ -4,6 +4,4 @@ import { swaggerSpec } from '../../docs/swagger';
 
 export const router = Router();
 
-console.log({ swaggerSpec });
-
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
